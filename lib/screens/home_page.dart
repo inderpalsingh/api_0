ba379571270f9eed79cb121ffd0bcdf782814ed7
Inderpal Singh <inderpalsmutta@gmail.com>
2024-05-19 19:50:41 +0530

    Latest

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,14 +1,13 @@
 import 'dart:convert';
 
 import 'package:api_0/models/main_model.dart';
-import 'package:api_0/models/posts_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 
 
 class HomePage extends StatefulWidget {
-  
+
 
 
   @override
@@ -17,16 +16,16 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   MainModel? mainData;
-  
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     getData();
-    
+
   }
-  
-  
+
+
   @override
   Widget build(BuildContext context) {
     print(mainData);
@@ -45,21 +44,21 @@ class _HomePageState extends State<HomePage> {
       ): const Center(child: Text('No posts')),
     );
   }
-  
-  
-  
+
+
+
   getData()async{
     String baseUrl = "https://dummyjson.com/posts";
     var response = await http.get(Uri.parse(baseUrl));
-    
+
     if(response.statusCode==200){
       var mData = response.body;
       print(mData);
-      
+
       var rawData = jsonDecode(mData);
       mainData = MainModel.fromJson(rawData);
       setState(() {
-        
+
       });
     }
   }
